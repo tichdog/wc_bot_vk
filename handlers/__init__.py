b7f96f7e20b9ba99e2299feb4264c647b93e2245
Юрий Силенок <yuri.silenok@gmail.com>
2026-04-16 07:39:59 +0000

    1

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,11 +1,13 @@
 from vkbottle.bot import Bot
 from handlers.admin import register_admin_handlers, register_admin_state_handlers_all
-from handlers.user import register_user_handlers
+from handlers import user
 from handlers.common import register_common_handlers
 
 
 def register_all_handlers(bot: Bot):
     register_admin_handlers(bot)
-    register_user_handlers(bot)
+    bot.set_blueprints(
+        user.bp,
+    )
     register_admin_state_handlers_all(bot)
     register_common_handlers(bot)
